1d7b372b95bb9d1b820f069c372fc9e0ad07d070
Vasty Terrel Villanueva <73398609+VastyEsthef@users.noreply.github.com>
2025-02-09 13:21:03 -0500

    volviendo al estado anterior

--- src/main/java/edu/pe/example/Patrones.java
+++ src/main/java/edu/pe/example/Patrones.java
@@ -17,7 +17,7 @@ public class Patrones {
             System.out.println();
         }
         for (int i = filas - 1; i > 0; i--) {
-            for (int j = 1; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
